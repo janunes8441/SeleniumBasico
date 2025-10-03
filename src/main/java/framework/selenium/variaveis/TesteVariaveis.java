8f39aef145f6767d70d27098a590da73ed63b42f
janunes8441 <janunes8441@gmail.com>
2025-10-03 09:38:07 -0300

    Novo projeto selenium substituindo o antigo

---
+++ src/main/java/framework/selenium/variaveis/TesteVariaveis.java
@@ -0,0 +1,71 @@
+package framework.selenium.variaveis;
+
+
+public class TesteVariaveis {
+	
+	public static String curso = "Variavel Global";
+	
+	public static void main(String[] args) {
+		//testeVariaveis();
+		testeVariavelEstatica();
+	}
+	
+	public static void testeVariaveis(){
+		
+		
+	String curso = "Variavel Local";
+	
+	VariaveisNaoEstaticas naoEstatica1 = new VariaveisNaoEstaticas();
+	VariaveisNaoEstaticas naoEstatica2 = new VariaveisNaoEstaticas();
+	VariaveisNaoEstaticas naoEstatica3 = new VariaveisNaoEstaticas();
+	VariaveisNaoEstaticas naoEstatica4 = new VariaveisNaoEstaticas();
+	
+	System.out.println("-------------Antes-------------");
+	
+	System.out.println(naoEstatica1.nome);
+	System.out.println(naoEstatica2.nome);
+	System.out.println(naoEstatica3.nome);
+	System.out.println(naoEstatica4.nome);
+	
+	System.out.println(curso);
+	
+	naoEstatica2.nome="Curso basico Java";
+	
+	System.out.println("-------------Depois-------------");
+	
+	System.out.println(naoEstatica1.nome);
+	System.out.println(naoEstatica2.nome);
+	System.out.println(naoEstatica3.nome);
+	System.out.println(naoEstatica4.nome);
+	
+}
+
+	public static void testeVariavelEstatica() {
+		
+		
+		VariaveisEstaticas estatica1 = new VariaveisEstaticas();
+		VariaveisEstaticas estatica2 = new VariaveisEstaticas();
+		VariaveisEstaticas estatica3 = new VariaveisEstaticas();
+		VariaveisEstaticas estatica4 = new VariaveisEstaticas();
+		
+		System.out.println("-------------Antes-------------");
+		
+		System.out.println(estatica1.cor);
+		System.out.println(estatica2.cor);
+		System.out.println(estatica3.cor);
+		System.out.println(estatica4.cor);
+		
+		System.out.println(curso);
+		
+		estatica2.cor="Vermelho Ferrari";
+		
+		System.out.println("-------------Depois-------------");
+		
+		System.out.println(estatica1.cor);
+		System.out.println(estatica2.cor);
+		System.out.println(estatica3.cor);
+		System.out.println(estatica4.cor);
+	}
+		
+		
+	}
