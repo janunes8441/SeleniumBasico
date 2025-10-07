a67af5d4366f0525e66099774e85a206d0835eb8
janunes8441 <janunes8441@gmail.com>
2025-10-07 09:22:15 -0300

    laços de repetição FOR

---
+++ src/main/java/framework/selenium/loop/Loops.java
@@ -0,0 +1,22 @@
+package framework.selenium.loop;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		testeFor(5);
+
+	}
+	
+	public static void testeFor(int maxLoop) {
+		
+		for (int i=0;i<=maxLoop;++i) {
+			System.out.println("Numero: "+i);
+		}
+		
+		
+		
+	}
+	
+	
+
+}
