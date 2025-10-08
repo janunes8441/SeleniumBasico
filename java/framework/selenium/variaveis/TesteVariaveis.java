b7809b35ab918ab60499a506ea3c8f1754ae85d1
janunes8441 <janunes8441@gmail.com>
2025-10-08 10:10:18 -0300

    ajuste remover warnings

--- java/framework/selenium/variaveis/TesteVariaveis.java
+++ java/framework/selenium/variaveis/TesteVariaveis.java
@@ -1,71 +1,84 @@
 package framework.selenium.variaveis;
 
-
+/**
+ * Classe de teste para demonstrar o comportamento de:
+ * - Variáveis de instância (não estáticas)
+ * - Variáveis de classe (estáticas)
+ */
 public class TesteVariaveis {
-	
-	public static String curso = "Variavel Global";
-	
-	public static void main(String[] args) {
-		testeVariaveis();
-		testeVariavelEstatica();
-	}
-	
-	public static void testeVariaveis(){
-		
-		
-	String curso = "Variavel Local";
-	
-	VariaveisNaoEstaticas naoEstatica1 = new VariaveisNaoEstaticas();
-	VariaveisNaoEstaticas naoEstatica2 = new VariaveisNaoEstaticas();
-	VariaveisNaoEstaticas naoEstatica3 = new VariaveisNaoEstaticas();
-	VariaveisNaoEstaticas naoEstatica4 = new VariaveisNaoEstaticas();
-	
-	System.out.println("-------------testeVariaveis Antes-------------");
-	
-	System.out.println(naoEstatica1.nome);
-	System.out.println(naoEstatica2.nome);
-	System.out.println(naoEstatica3.nome);
-	System.out.println(naoEstatica4.nome);
-	
-	System.out.println(curso);
-	
-	naoEstatica2.nome="Curso basico Java";
-	
-	System.out.println("-------------testeVariaveis Depois-------------");
-	
-	System.out.println(naoEstatica1.nome);
-	System.out.println(naoEstatica2.nome);
-	System.out.println(naoEstatica3.nome);
-	System.out.println(naoEstatica4.nome);
-	
-}
 
-	public static void testeVariavelEstatica() {
-		
-		
-		VariaveisEstaticas estatica1 = new VariaveisEstaticas();
-		VariaveisEstaticas estatica2 = new VariaveisEstaticas();
-		VariaveisEstaticas estatica3 = new VariaveisEstaticas();
-		VariaveisEstaticas estatica4 = new VariaveisEstaticas();
-		
-		System.out.println("-------------testeVariavelEstatica Antes-------------");
-		
-		System.out.println(estatica1.cor);
-		System.out.println(estatica2.cor);
-		System.out.println(estatica3.cor);
-		System.out.println(estatica4.cor);
-		
-		System.out.println(curso);
-		
-		estatica2.cor="Vermelho Ferrari";
-		
-		System.out.println("-------------testeVariavelEstatica Depois-------------");
-		
-		System.out.println(estatica1.cor);
-		System.out.println(estatica2.cor);
-		System.out.println(estatica3.cor);
-		System.out.println(estatica4.cor);
-	}
-		
-		
-	}
+    // Variável global (estática) da classe
+    public static String curso = "Variavel Global";
+
+    public static void main(String[] args) {
+        testeVariaveis();         // Testa variáveis de instância
+        testeVariavelEstatica();  // Testa variáveis estáticas
+    }
+
+    /**
+     * Demonstra o comportamento de variáveis não estáticas (instância)
+     * Cada objeto possui sua própria cópia da variável
+     */
+    public static void testeVariaveis() {
+
+        // Variável local do método
+        String curso = "Variavel Local";
+
+        // Criando várias instâncias de VariaveisNaoEstaticas
+        VariaveisNaoEstaticas naoEstatica1 = new VariaveisNaoEstaticas();
+        VariaveisNaoEstaticas naoEstatica2 = new VariaveisNaoEstaticas();
+        VariaveisNaoEstaticas naoEstatica3 = new VariaveisNaoEstaticas();
+        VariaveisNaoEstaticas naoEstatica4 = new VariaveisNaoEstaticas();
+
+        System.out.println("-------------testeVariaveis Antes-------------");
+
+        // Cada instância possui seu próprio valor da variável 'nome'
+        System.out.println(naoEstatica1.nome);
+        System.out.println(naoEstatica2.nome);
+        System.out.println(naoEstatica3.nome);
+        System.out.println(naoEstatica4.nome);
+
+        // Mostra a variável local do método
+        System.out.println(curso);
+
+        // Alterando o valor da variável de uma instância
+        naoEstatica2.nome = "Curso basico Java";
+
+        System.out.println("-------------testeVariaveis Depois-------------");
+
+        // Mostra que apenas a instância alterada mudou
+        System.out.println(naoEstatica1.nome);
+        System.out.println(naoEstatica2.nome);
+        System.out.println(naoEstatica3.nome);
+        System.out.println(naoEstatica4.nome);
+    }
+
+    /**
+     * Demonstra o comportamento de variáveis estáticas
+     * Todas as instâncias compartilham a mesma cópia da variável
+     */
+    public static void testeVariavelEstatica() {
+
+        System.out.println("-------------testeVariavelEstatica Antes-------------");
+
+        // Acessando a variável estática diretamente pela classe
+        System.out.println(VariaveisEstaticas.cor);
+        System.out.println(VariaveisEstaticas.cor);
+        System.out.println(VariaveisEstaticas.cor);
+        System.out.println(VariaveisEstaticas.cor);
+
+        // Mostra a variável global
+        System.out.println(curso);
+
+        // Alterando a variável estática
+        VariaveisEstaticas.cor = "Vermelho Ferrari";
+
+        System.out.println("-------------testeVariavelEstatica Depois-------------");
+
+        // Todas as instâncias refletem a alteração, pois é estática
+        System.out.println(VariaveisEstaticas.cor);
+        System.out.println(VariaveisEstaticas.cor);
+        System.out.println(VariaveisEstaticas.cor);
+        System.out.println(VariaveisEstaticas.cor);
+    }
+}
