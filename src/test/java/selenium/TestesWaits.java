cd977cff7e4a9679ac27a6d2a31a3006bc9485e0
janunes8441 <janunes8441@gmail.com>
2025-10-20 15:43:29 -0300

    ajuste testes wait, retirado os warnings

--- src/test/java/selenium/TestesWaits.java
+++ src/test/java/selenium/TestesWaits.java
@@ -1,13 +1,13 @@
 package selenium;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+//import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
